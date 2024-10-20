02a5876c5f4a8fe06457ced5eb08b06d96548e22
TymonScibiorski <tym9128@gmail.com>
2024-10-20 18:25:34 +0200

    Tried solving the exercise, which will be added here later, nothing really done yet.

---
+++ main.py
@@ -0,0 +1,21 @@
+def condition(entry, limits, n):
+     a = entry[0]
+     b = entry[1]
+     c = entry[2]
+
+     return (a+b+c<=n and a<=limits[0], b <= limits[1], c <= limits[2])
+
+def single(limits):
+     
+
+if __name__ == '__main__':
+     x = int(1)
+     y = int(1)
+     z = int(2)
+     n = int(3)
+
+     limits = [x, y, z]
+
+
+
+     output_list = []
